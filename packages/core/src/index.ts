4cf4af6f837ec0c4fa43eb19a7cdb4fbe12e01c8
XLor <yjl9903@vip.qq.com>
2023-07-11 21:30:47 +0800

    feat(core): support custom aria2c

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,9 +7,15 @@ import { BINARY } from './binding.mjs';
 
 const require = createRequire(import.meta.url);
 
-export function run(args: string[], options: ExecaOptions = {}) {
-  const pkg = require.resolve(BINARY + '/package.json');
-  const { platform } = process;
-  const binary = path.join(path.dirname(pkg), platform === 'win32' ? 'aria2c.exe' : 'aria2c');
-  return execa(binary, args, options);
+export function run(args: string[], options: ExecaOptions & { binary?: string } = {}) {
+  if (!options.binary) {
+    const pkg = require.resolve(BINARY + '/package.json');
+    const { platform } = process;
+    const binary = path.join(path.dirname(pkg), platform === 'win32' ? 'aria2c.exe' : 'aria2c');
+    return execa(binary, args, options);
+  } else {
+    const binary = options.binary;
+    delete options['binary'];
+    return execa(binary, args, options);
+  }
 }
