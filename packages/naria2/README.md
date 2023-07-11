749abdd3efce4f1a165133b37073fab8a4a69ea6
yjl9903 <yjl9903@onekuma.cn>
2023-07-11 15:01:13 +0800

    chore: init repo

---
+++ packages/naria2/README.md
@@ -0,0 +1,5 @@
+# naria2
+
+## License
+
+MIT License © 2023 [XLor](https://github.com/yjl9903)
---
+++ packages/naria2/test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('hello', () => {
+  it('should work', () => {
+    expect('Hello').toEqual('Hello');
+  });
+});
