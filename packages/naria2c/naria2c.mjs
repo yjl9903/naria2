3273d4dbf489b1b16955507547ac1680bd225e28
XLor <yjl9903@vip.qq.com>
2023-07-11 23:49:04 +0800

    feat(naria2c): handle kill signal

--- packages/naria2c/naria2c.mjs
+++ packages/naria2c/naria2c.mjs
@@ -4,8 +4,11 @@ import { run } from '@naria2/core';
 
 try {
   const subprocess = await run(process.argv, { stdio: 'inherit' });
+
   if (subprocess.exitCode !== 0) {
     process.exit(subprocess.exitCode);
+  } else if (subprocess.signal !== undefined) {
+    process.kill(subprocess.signal);
   }
 } catch (error) {
   console.error(error);
