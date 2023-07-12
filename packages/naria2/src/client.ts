818ca6b5eeecfdb7c6a8051b1d24f7935251534c
XLor <yjl9903@vip.qq.com>
2023-07-13 01:11:24 +0800

    feat(naria2): close conn

--- packages/naria2/src/client.ts
+++ packages/naria2/src/client.ts
@@ -1,23 +1,29 @@
 import { type Socket, type Conn, type OpenOptions, open, close } from 'maria2';
 
 export class Aria2Client {
-  readonly _conn: Conn;
+  private _conn: Conn | undefined;
 
   constructor(conn: Conn) {
     this._conn = conn;
   }
 
   get conn(): Conn {
+    if (!this._conn) {
+      throw new Error('Connection has been closed');
+    }
     return this._conn;
   }
 
   get socket(): Socket {
-    return this._conn.getSocket();
+    return this.conn.getSocket();
   }
 
   async close() {
     close(this.conn);
+    this._conn = undefined;
   }
+
+  async download() {}
 }
 
 export async function createClient(_socket: Socket | Promise<Socket>, options: OpenOptions = {}) {
