c9cbe94f5c92e7b4f43bbf0c2638d945392c6d4f
XLor <yjl9903@vip.qq.com>
2023-07-13 01:26:24 +0800

    feat(naria2c): proxy help and version subcommand

--- packages/naria2c/naria2c.mjs
+++ packages/naria2c/naria2c.mjs
@@ -1,9 +1,35 @@
 #!/usr/bin/env node
 
 import { run } from '@naria2/node';
+import { readFile } from 'fs/promises';
 
 try {
-  const subprocess = await run(process.argv.slice(2), { stdio: 'inherit' });
+  const args = process.argv.slice(2);
+  const subprocess = run(args);
+
+  if (args.includes('-h') || args.includes('--help')) {
+    const result = await subprocess;
+    const help = result.stdout.replace(/aria2c/g, 'naria2c');
+    const { version } = await getPackage();
+
+    console.log(`naria2c/${version}`);
+    console.log();
+    console.log(`naria2c is a cross-platform wrapper of aria2c.`);
+    console.log();
+    console.log(help);
+  } else if (args.includes('-v') || args.includes('--version')) {
+    const result = await subprocess;
+    const content = result.stdout;
+    const { version } = await getPackage();
+
+    console.log(`naria2c/${version}`);
+    console.log(`--------------------`);
+    console.log(content);
+  } else {
+    subprocess.pipeStdout(process.stdout);
+    subprocess.pipeStderr(process.stderr);
+    await subprocess;
+  }
 
   if (subprocess.exitCode !== 0) {
     process.exit(subprocess.exitCode);
@@ -13,4 +39,8 @@ try {
 } catch (error) {
   console.error(error);
   process.exit(1);
+}
+
+async function getPackage() {
+  return JSON.parse(await readFile(new URL('./package.json', import.meta.url)));
 }
