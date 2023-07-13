759503cbb5c06bea852615498a18d17c188924fb
XLor <yjl9903@vip.qq.com>
2023-07-13 22:29:09 +0800

    feat(naria2c): add test

--- packages/naria2c/test/index.test.ts
+++ packages/naria2c/test/index.test.ts
@@ -1,8 +1,16 @@
 import { execa } from 'execa';
 import { describe, it, expect } from 'vitest';
 
+import { version } from '../package.json';
+
 describe('naria2c', () => {
-  it('should work', async () => {
-    // await execa('node', ['./naria2c.mjs', '--version']);
+  it('should output version', async () => {
+    const result = await execa('node', ['./naria2c.mjs', '--version']);
+    expect(result.stdout.split('\n')[0]).toBe(`naria2c/${version}`);
+  });
+
+  it('should output help', async () => {
+    const result = await execa('node', ['./naria2c.mjs', '-h']);
+    expect(result.stdout.split('\n')[0]).toBe(`naria2c/${version}`);
   });
 });
