f76659e86821bb3ab5e35fceeaba4d545ba704b1
XLor <yjl9903@vip.qq.com>
2023-07-13 22:34:35 +0800

    chore: enable test

--- packages/naria2/test/index.test.ts
+++ packages/naria2/test/index.test.ts
@@ -6,13 +6,13 @@ describe('spawn aria2 subprocess', () => {
   let conn: SubprocessSocket;
 
   it('should work', async () => {
-    // conn = await createSubprocess();
-    // const ok = await new Promise<boolean>((r) => {
-    //   conn.addEventListener('open', () => r(true));
-    //   conn.addEventListener('error', () => r(false));
-    //   conn.addEventListener('close', () => r(false));
-    // });
-    // expect(ok).toBeTruthy();
+    conn = await createSubprocess();
+    const ok = await new Promise<boolean>((r) => {
+      conn.addEventListener('open', () => r(true));
+      conn.addEventListener('error', () => r(false));
+      conn.addEventListener('close', () => r(false));
+    });
+    expect(ok).toBeTruthy();
   });
 
   afterAll(() => {
