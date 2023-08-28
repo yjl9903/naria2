278cdf799743f2ec990af4ca1e6950f6fcca55ee
XLor <yjl9903@vip.qq.com>
2023-08-28 23:33:57 +0800

    fix: receive string[]

--- packages/options/src/stringify.ts
+++ packages/options/src/stringify.ts
@@ -2,9 +2,18 @@ import type { Aria2ClientGlobalOptionKey, Aria2ClientInputOptionKey } from 'mari
 
 import type { Aria2RPCOptionsKey } from './types';
 
+// See https://twitter.com/mattpocockuk/status/1622730173446557697
+// export type Identity<T> = T;
+// type Prettify<T> = Identity<{ [K in keyof T]: T[K] }>
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
 export function stringifyCliOptions(
-  options: Partial<Record<Aria2ClientInputOptionKey & Aria2ClientGlobalOptionKey, string>> &
-    Partial<Record<Aria2RPCOptionsKey, string>>
+  options: Prettify<
+    Partial<Record<Aria2ClientInputOptionKey & Aria2ClientGlobalOptionKey, string | string[]>> &
+      Partial<Record<Aria2RPCOptionsKey, string | string[]>>
+  >
 ) {
   return Object.entries({ ...options }).flatMap(([k, v]) => {
     if (Array.isArray(v)) {
