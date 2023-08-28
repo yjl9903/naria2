4f0ab107ce70f9e158e01eb85f9114a91ed625ad
XLor <yjl9903@vip.qq.com>
2023-08-28 23:57:50 +0800

    chore: play alias

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,3 +1,6 @@
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig } from 'vite';
 
 import Naria2 from 'vite-plugin-naria2';
@@ -5,6 +8,15 @@ import BuildInfo from 'vite-plugin-info';
 import TopLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      naria2: path.resolve(fileURLToPath(import.meta.url), '../../packages/naria2/src/index.ts'),
+      '@naria2/options': path.resolve(
+        fileURLToPath(import.meta.url),
+        '../../packages/options/src/index.ts'
+      )
+    }
+  },
   plugins: [
     BuildInfo(),
     TopLevelAwait(),
