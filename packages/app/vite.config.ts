b3ce0d765996a2ee617de6727864a96818f2d948
XLor <yjl9903@vip.qq.com>
2023-10-29 14:49:58 +0800

    fix(app): build

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -7,14 +7,13 @@ import Naria2 from 'vite-plugin-naria2';
 import BuildInfo from 'vite-plugin-info';
 import TopLevelAwait from 'vite-plugin-top-level-await';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   resolve: {
     alias: {
-      naria2: path.resolve(fileURLToPath(import.meta.url), '../../packages/naria2/src/index.ts'),
-      '@naria2/options': path.resolve(
-        fileURLToPath(import.meta.url),
-        '../../packages/options/src/index.ts'
-      )
+      naria2: path.resolve(__dirname, '../naria2/src/index.ts'),
+      '@naria2/options': path.resolve(__dirname, '../options/src/index.ts')
     }
   },
   plugins: [
