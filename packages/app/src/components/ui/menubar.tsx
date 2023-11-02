9f103e8d4411744ff29bba1c804268d7c585413d
XLor <yjl9903@vip.qq.com>
2023-11-02 23:15:46 +0800

    fix(app): menubar align

--- packages/app/src/components/ui/menubar.tsx
+++ packages/app/src/components/ui/menubar.tsx
@@ -20,7 +20,7 @@ const Menubar = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <MenubarPrimitive.Root
     ref={ref}
-    className={cn('flex h-10 items-center space-x-1 rounded-md bg-background p-1', className)}
+    className={cn('flex h-10 items-center space-x-1 rounded-md bg-background py-1', className)}
     {...props}
   />
 ));
@@ -35,7 +35,7 @@ const MenubarButton = ({
 }) => (
   <button
     className={cn(
-      'flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground hover:bg-accent',
+      'flex cursor-default select-none items-center rounded-sm px-4 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground hover:bg-accent',
       className
     )}
     {...props}
